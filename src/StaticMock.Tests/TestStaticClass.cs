bb0a08830598301f04b39b27a53250cc879b542d
SvetlovA <svetlov.arte@gmail.com>
2021-04-23 20:43:42 +0300

    added tests for returns

--- src/StaticMock.Tests/TestStaticClass.cs
+++ src/StaticMock.Tests/TestStaticClass.cs
@@ -19,9 +19,21 @@ namespace StaticMock.Tests
             return await Task.FromResult(1);
         }
 
-        public static TestInstance TestMethodReturnInstanceObject()
+        public static TestInstance TestMethodReturnReferenceObject()
         {
             return new();
+        }
+
+        public static int TestMethodReturn1WithOutParameter(out int x)
+        {
+            x = 1;
+            return 1;
+        }
+
+        public static int TestMethodReturn1WithRefParameter(ref int x)
+        {
+            x = 1;
+            return 1;
         }
     }
 }
