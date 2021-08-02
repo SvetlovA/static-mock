49175bca2072a40e02df798e94a0745087a5e852
SvetlovA <svetlov.arte@gmail.com>
2021-08-02 17:51:39 +0300

    added tests for setup

---
+++ src/StaticMock.Tests/SetupTests/GenericSetupTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using StaticMock.Tests.TestEntities;
+
+namespace StaticMock.Tests.SetupTests
+{
+    [TestFixture]
+    public class GenericSetupTests
+    {
+        [Test]
+        public void TestGenericSetup()
+        {
+            Mock.Setup(() => TestStaticClass.TestMethodReturn1WithoutParameters(), () => { });
+        }
+    }
+}
---
+++ src/StaticMock.Tests/SetupTests/SetupTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+using StaticMock.Tests.TestEntities;
+
+namespace StaticMock.Tests.SetupTests
+{
+    [TestFixture]
+    public class SetupTests
+    {
+        [Test]
+        public void TestSetup()
+        {
+            Mock.Setup(typeof(TestStaticClass), nameof(TestStaticClass.TestMethodReturn1WithoutParameters), () => { });
+        }
+
+        [Test]
+        public void TestSetupVoid()
+        {
+            Mock.SetupVoid(typeof(TestStaticClass), nameof(TestStaticClass.TestVoidMethodWithParameters), () => { });
+        }
+
+        [Test]
+        public void TestSetupProperty()
+        {
+            Mock.SetupProperty(typeof(TestStaticClass), nameof(TestStaticClass.StaticIntProperty), () => { });
+        }
+
+        [Test]
+        public void TestSetupNegativeNotExistingMethodName()
+        {
+            Assert.Throws<Exception>(() => Mock.Setup(typeof(TestStaticClass), "abc123", () => { }));
+        }
+
+        [Test]
+        public void TestSetupNegativeVoid()
+        {
+            Assert.Throws<Exception>(() => Mock.Setup(typeof(TestStaticClass),  nameof(TestStaticClass.TestVoidMethodWithParameters), () => { }));
+        }
+
+        [Test]
+        public void TestSetupMethodNegativeProperty()
+        {
+            Assert.Throws<Exception>(() => Mock.Setup(typeof(TestStaticClass), nameof(TestStaticClass.StaticIntProperty), () => { }));
+        }
+
+        [Test]
+        public void TestSetupPropertyNegativeMethod()
+        {
+            Assert.Throws<Exception>(() => Mock.SetupProperty(typeof(TestStaticClass),  nameof(TestStaticClass.TestMethodReturn1WithoutParameters), () => { }));
+        }
+    }
+}
