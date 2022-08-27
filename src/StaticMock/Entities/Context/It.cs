4baa78dc79e8b03607c2b9ac10689a20d683c710
SvetlovA <svetlov.arte@gmail.com>
2022-08-27 22:16:26 +0300

    - removed debug logs from It implementation - bump version - updated copyright year

--- src/StaticMock/Entities/Context/It.cs
+++ src/StaticMock/Entities/Context/It.cs
@@ -1,5 +1,4 @@
 ﻿using System.Linq.Expressions;
-using System.Reflection;
 
 namespace StaticMock.Entities.Context;
 
@@ -17,14 +16,6 @@ public class It
     public TValue? Is<TValue>(Expression<Func<TValue, bool>> predicate)
     {
         var predicateParameterExpression = predicate.Parameters[0];
-
-        var writeLineExpression = Expression.Call(null, typeof(Console).GetMethod(
-            "WriteLine",
-            BindingFlags.Public | BindingFlags.Static,
-            null,
-            new[] { typeof(TValue) },
-            Array.Empty<ParameterModifier>()), predicateParameterExpression);
-
         var conditionExpression = Expression.IfThen(
             Expression.Not(predicate.Body),
             Expression.Throw(Expression.Constant(new Exception($"Condition {predicate} failed."))));
@@ -32,11 +23,7 @@ public class It
         _setupContextState.ItParameterExpressions.Add(new ItParameterExpression
         {
             ParameterType = predicateParameterExpression.Type,
-            ParameterExpression = Expression.Lambda<Action<TValue>>(Expression.Block(new Expression[]
-            {
-                writeLineExpression,
-                conditionExpression
-            }), predicateParameterExpression)
+            ParameterExpression = Expression.Lambda<Action<TValue>>(conditionExpression, predicateParameterExpression)
         });
 
         return default;
